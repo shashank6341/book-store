4e19a3cf1ca98ca60b2681bbed7e0c7abc0af685
Shashank Verma <shashank6341@gmail.com>
2021-01-06 08:12:29 +0530

    Added Cart Functionality

--- models/product.js
+++ models/product.js
@@ -1,58 +1,27 @@
 const Sequelize = require('sequelize');
 
-const sequelize = require("../util/database");
+const sequelize = require('../util/database');
 
 const Product = sequelize.define('product', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
-    allowNull: true,
-    primaryKey: true,
+    allowNull: false,
+    primaryKey: true
   },
   title: Sequelize.STRING,
   price: {
     type: Sequelize.DOUBLE,
-    allowNull: false,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   },
   description: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   }
 });
 
 module.exports = Product;
-
-
-
-// const Cart = require("./cart");
-
-// module.exports = class Product {
-//   constructor(id, title, imageUrl, description, price) {
-//     this.id = id;
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   save() {
-//     return db.execute(
-//       "INSERT INTO products (title, price, description, imageUrl) VALUES (?,?,?,?)",
-//       [this.title, this.price, this.description, this.imageUrl]
-//     );
-//   }
-
-//   static deleteById(id) {}
-
-//   static fetchAll() {
-//     return db.execute("SELECT * FROM products");
-//   }
-
-//   static findById(id) {
-//     return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-//   }
-// };
--- util/database.js
+++ util/database.js
@@ -1,23 +1,8 @@
-// const mysql = require('mysql2');
-
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'node-complete',
-//     password: 'Pikachu@1998'
-// });
-
-// module.exports = pool.promise();
-
-// --------------- Sequelize-----------------
-
-const Sequelize = require('sequelize').Sequelize;
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', 'password', {
-  dialect: "mysql",
-  host: "localhost",
+  dialect: 'mysql',
+  host: 'localhost'
 });
-
-// This also creates a connection pool
 
 module.exports = sequelize;
