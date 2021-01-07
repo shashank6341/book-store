6495438d20a94f244808caafc1e8ba15f4f7d356
Shashank Verma <shashank6341@gmail.com>
2021-01-07 20:17:01 +0530

    Pre-Cart Implementation

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,6 +18,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
+    userId: req.user._id,
   });
   product
     .save()
@@ -76,6 +77,8 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('name title -_id')
+    // .populate('userId', 'name')
     .then((products) => {
       res.render("admin/products", {
         prods: products,
