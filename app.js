8f62a365d0590f041048c8cd896de0ffbfeadeef
Shashank Verma <shashank6341@gmail.com>
2021-01-16 11:37:14 +0530

    Minor Changes

--- app.js
+++ app.js
@@ -57,6 +57,9 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
+  if (req.session.isLoggedIn === true) {
+    res.locals.loggedInUser = req.session.user.email;
+  }
   next();
 });
 
